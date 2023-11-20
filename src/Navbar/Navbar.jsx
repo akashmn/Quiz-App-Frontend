be48cc81d2ff173e4f6ab0ddf7bec3137974cdb0
akashmn <akash003nandan@gmail.com>
2023-11-21 05:07:57 +0530

    navbar active

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
+
+  const [active, setActive] = useState('home')
+
   return (
     <div className="navbar">
         <div className="logo">QUIZINGO</div>
         <div className="menu">
-            <div className="home"><a href="">Home</a></div>
-            <div className="leaderboard"><a href="">Leaderboard</a></div>
+            <div className="home"><a href="#" onClick={()=> setActive('home')} className={active === 'home' ? 'active' : '' }>Home</a></div>
+            <div className="leaderboard"><a href="#" onClick={()=> setActive('leaderboard')} className={active === 'leaderboard' ? 'active' : '' }>Leaderboard</a></div>
         </div>
         <div className="logout">Logout</div>
     </div>
